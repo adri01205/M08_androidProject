87bed60f2580136d55324da55313fa08bfc08499
Sergio <serjimartsergio@gmail.com>
2016-02-22 19:10:15 +0100

    Login layout done

---
+++ app/src/main/java/com/example/adri/m08_androidproject/controller/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.adri.m08_androidproject.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.adri.m08_androidproject.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
--- app/src/main/java/com/example/adri/m08_androidproject/controller/hola2.java
+++
@@ -1,7 +0,0 @@
-package com.example.adri.m08_androidproject.controller;
-
-/**
- * Created by adri on 22/02/2016.
- */
-public class hola2 {
-}
