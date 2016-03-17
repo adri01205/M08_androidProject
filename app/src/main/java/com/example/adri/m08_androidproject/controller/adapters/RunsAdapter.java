b557eff7997d381e4bc8f28cdd063d69a2e35bca
adri01205 <adri01205@gmail.com>
2016-03-17 17:02:46 +0100

    spinner

---
+++ app/src/main/java/com/example/adri/m08_androidproject/controller/adapters/RunsAdapter.java
@@ -0,0 +1,80 @@
+package com.example.adri.m08_androidproject.controller.adapters;
+
+import android.app.Activity;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+import com.example.adri.m08_androidproject.R;
+import com.example.adri.m08_androidproject.model.business.Run;
+
+
+public class RunsAdapter extends BaseAdapter {
+    private Activity context;
+    private Cursor dades;
+
+    /**
+     * Constructor
+     * @param context el context de l'aplicaci�
+     * @param dades cursor amb les dades
+     */
+    public RunsAdapter(Activity context, Cursor dades) {
+        super();
+        this.context = context;
+        this.dades = dades;
+    }
+
+    /**
+     * Sobreescriptura del m�tode getView per indicar com s'han de mostrar
+     * les dades d'una fila del ListView
+     */
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View element = convertView;
+
+        Run r = getItem(position);
+
+        if(element == null) {
+            LayoutInflater inflater = context.getLayoutInflater();
+            element = inflater.inflate(R.layout.run_adapter, null);
+        }
+
+        ((TextView) element.findViewById(R.id.tvDate)).setText(r.getRun_date());
+        ((TextView) element.findViewById(R.id.tvTime)).setText(r.getRun_time());
+
+        return element;
+    }
+
+    /**
+     * Sobreescriptura del m�tode getCount que indica quantes dades gestiona
+     * l'adaptador.
+     */
+    public int getCount() {
+
+        return dades.getCount();
+    }
+    /**
+     * Sobreescriptura del m�tode getItem que retorna l'objecte que ocupa la
+     * posici� indicada amb el par�metre.
+     */
+    public Run getItem(int pos) {
+        Run r = new Run();
+        if(dades.moveToPosition(pos)) {
+
+            r.setId(dades.getInt(0));
+            r.setRun_time(dades.getString(1));
+            r.setRun_date(dades.getString(2));
+            r.setId_user(dades.getInt(3));
+        }
+        return r;
+    }
+    /**
+     * Sobreescriptura del m�tode getItemId que retorna l'id de l'objecte
+     * que ocupa la posici� indicad amb el par�metre.
+     */
+    public long getItemId(int position) {
+
+        return getItem(position).getId();
+    }
+}
