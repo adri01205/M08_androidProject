6da9d13563e53ea96bb0ba4717011fd86a104106
Sergio <serjimartsergio@gmail.com>
2016-03-22 22:27:14 +0100

    Cronometro por service.

--- app/src/main/java/com/example/adri/m08_androidproject/controller/ChronoActivity.java
+++ app/src/main/java/com/example/adri/m08_androidproject/controller/ChronoActivity.java
@@ -1,15 +1,19 @@
 package com.example.adri.m08_androidproject.controller;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
-import android.os.Handler;
+import android.support.v4.content.LocalBroadcastManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.example.adri.m08_androidproject.R;
+import com.example.adri.m08_androidproject.controller.service.CronoService;
 
 /**
  * Created by Sergio on 28/02/2016.
@@ -20,27 +24,30 @@ public class ChronoActivity extends Activity implements View.OnClickListener {
     ImageButton startStop;
     Button cancel, save;
     Boolean stoped = true;
-    //timer
-    private Handler timerHandler = new Handler();
-    private int seconds = 00, min = 00;
-    private Runnable timerRunnable = new Runnable() {
+    Intent intentChronoService;
 
-        @Override
-        public void run() {
+    /*
+        //timer
+        private Handler timerHandler = new Handler();
+        private int seconds = 00, min = 00;
+        private Runnable timerRunnable = new Runnable() {
 
-            if (stoped == false) {
-                seconds++;
-                second.setText(String.valueOf(seconds));
-                minute.setText(String.valueOf(min));
-                if (seconds == 59) {
-                    seconds = 00;
-                    min++;
+            @Override
+            public void run() {
+
+                if (stoped == false) {
+                    seconds++;
+                    second.setText(String.valueOf(seconds));
+                    minute.setText(String.valueOf(min));
+                    if (seconds == 59) {
+                        seconds = 00;
+                        min++;
+                    }
+                    timerHandler.postDelayed(this, 1000);
                 }
-                timerHandler.postDelayed(this, 1000);
             }
-        }
-    };
-
+        };
+    */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,7 +63,19 @@ public class ChronoActivity extends Activity implements View.OnClickListener {
         save.setOnClickListener(this);
         cancel.setOnClickListener(this);
 
+        intentChronoService = new Intent(this, CronoService.class);
+        startService(intentChronoService); //Iniciar servicio
 
+        // Filtro de acciones que serán alertadas
+        IntentFilter filter = new IntentFilter("time");
+        filter.addAction("seconds");
+        filter.addAction("minutes");
+
+
+        // Crear un nuevo ResponseReceiver
+        ResponseReceiver receiver = new ResponseReceiver();
+        // Registrar el receiver y su filtro
+        LocalBroadcastManager.getInstance(this).registerReceiver(receiver, filter);
     }
 
     @Override
@@ -90,25 +109,48 @@ public class ChronoActivity extends Activity implements View.OnClickListener {
 
     }
 
+
     @Override
     public void onClick(View v) {
-        Intent intent;
         if (v.getId() == R.id.btn_save) {
+            stopService(intentChronoService); // Detener servicio
 
         } else if (v.getId() == R.id.btn_cancel) {
-            intent = new Intent(this, MenuActivity.class);
-            startActivity(intent);
-        } else if (v.getId() == R.id.ibtn_startStop) {
+            stopService(intentChronoService); // Detener servicio
+
+            startActivity(new Intent(this, MenuActivity.class));
+
+        } else if (v.getId() == R.id.ibtn_startStop) { //////////////////////////////////////////////TO DO Stop and Start time in Service  when button is clicked
             if (stoped == true) {
                 stoped = false;
                 startStop.setImageResource(R.mipmap.stop);
-                timerHandler.postDelayed(timerRunnable, 0);
+
+
+                //timerHandler.postDelayed(timerRunnable, 0);
             } else {
                 stoped = true;
                 startStop.setImageResource(R.mipmap.start);
-
             }
         }
+    }
 
+
+    // Broadcast receiver que recibe las emisiones desde los servicios
+    private class ResponseReceiver extends BroadcastReceiver {
+
+        // Sin instancias
+        private ResponseReceiver() {
+        }
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            switch (intent.getAction()) {
+                case "time":
+                    second.setText(intent.getStringExtra("seconds"));
+                    minute.setText(intent.getStringExtra("minutes"));
+                    break;
+            }
+        }
     }
 }
